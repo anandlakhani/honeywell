6b70c5739565a1a35ac9f9ad8841010434f68fba
anandlakhani <anand.lakhani@gmail.com>
2015-12-10 17:34:42 -0500

    Update app.rb

--- honeywell-oauth-redirect-master/app.rb
+++ honeywell-oauth-redirect-master/app.rb
@@ -2,11 +2,11 @@ require 'sinatra'
 require 'rest-client'
 require 'json'
 
+before do
+	content_type 'application/json'
+end
 
-
-
-begin
-acxt
+get '/redirect' do
   code = params[:code]
   @auth = ENV['basic_auth']
   auth_get = 'grant_type=authorization_code&code=' + code + '&redirect_uri=' + ENV['redirect_uri']
