7557e4e4eefe2ca0910ce38772c0582a5643f66d
anandlakhani <anand.lakhani@gmail.com>
2015-12-10 17:33:21 -0500

    Update app.rb

--- honeywell-oauth-redirect-master/app.rb
+++ honeywell-oauth-redirect-master/app.rb
@@ -6,7 +6,7 @@ require 'json'
 
 
 begin
-
+acxt
   code = params[:code]
   @auth = ENV['basic_auth']
   auth_get = 'grant_type=authorization_code&code=' + code + '&redirect_uri=' + ENV['redirect_uri']
