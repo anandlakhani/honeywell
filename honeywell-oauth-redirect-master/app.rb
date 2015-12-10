291cad88663f0cd1be1be1b7275bd659645e93f7
anandlakhani <anand.lakhani@gmail.com>
2015-12-10 17:31:56 -0500

    Update app.rb

--- honeywell-oauth-redirect-master/app.rb
+++ honeywell-oauth-redirect-master/app.rb
@@ -11,8 +11,7 @@ begin
   @auth = ENV['basic_auth']
   auth_get = 'grant_type=authorization_code&code=' + code + '&redirect_uri=' + ENV['redirect_uri']
   response = RestClient.post('https://qtccna.honeywell.com/sandbox/Auth/OAuth/token', auth_get, {:authorization => @auth, :accept => 'json', :content_type => 'application/x-www-form-urlencoded'})
-  response = JSON.parse(response)
-  response.to_json ## Spits out the data to the page in json
+  puts "#{response.to_str}"
 rescue => e
    puts "ERROR: #{e}"
  end 
