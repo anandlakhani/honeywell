46b6439a9ef1629fda2feb60522d1d89847c7ae0
anandlakhani <anand.lakhani@gmail.com>
2015-12-10 17:07:42 -0500

    Update app.rb

--- honeywell-oauth-redirect-master/app.rb
+++ honeywell-oauth-redirect-master/app.rb
@@ -9,11 +9,7 @@ end
 get '/' do
   "Hello World!"
   
-  code = params[:code]
-  auth_get = 'grant_type=authorization_code&code=' + code + '&redirect_uri=https://honeywell.herokuapp.com/redirect'
-  response = RestClient.post('http://www.rediff.com', auth_get, { :accept => 'json', :content_type => 'text/html'})
-  response = JSON.parse(response)
-  response.to_json
+ 
 end
 
 
