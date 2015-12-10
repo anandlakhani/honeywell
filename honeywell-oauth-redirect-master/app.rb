7d9befe7ee6d3b15a472732041e094b2988d17c1
anandlakhani <anand.lakhani@gmail.com>
2015-12-10 17:37:35 -0500

    Update app.rb

--- honeywell-oauth-redirect-master/app.rb
+++ honeywell-oauth-redirect-master/app.rb
@@ -1,7 +1,11 @@
 require 'sinatra'
 require 'rest-client'
 require 'json'
-
+configure :development do
+  set :bind, '0.0.0.0'   
+  set :port, 3000
+  config_file './config.yml'
+end
 before do
 	content_type 'application/json'
 end
