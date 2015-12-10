5aaf1adc584cedf8bad634d32af4811df16f2d34
anandlakhani <anand.lakhani@gmail.com>
2015-12-10 16:10:14 -0500

    Update app.rb

--- honeywell-oauth-redirect-master/app.rb
+++ honeywell-oauth-redirect-master/app.rb
@@ -1,18 +1,6 @@
 require 'sinatra'
-require 'rest-client'
-require 'json'
 
 
- 
-before do
-	content_type 'application/json'
-end
-
-get '/redirect' do
-  code = params[:code]
-  @auth = ENV['basic_auth']
-  auth_get = 'grant_type=authorization_code&code=' + code + '&redirect_uri=https://honeywell.herokuapp.com/redirect'
-  response = RestClient.post('https://qtccna.honeywell.com/sandbox/Auth/OAuth/token', auth_get, {:authorization => @auth, :accept => 'json', :content_type => 'application/x-www-form-urlencoded'})
-  response = JSON.parse(response)
-  response.to_json ## Spits out the data to the page in json
+get '/' do
+  "Hello World!"
 end
