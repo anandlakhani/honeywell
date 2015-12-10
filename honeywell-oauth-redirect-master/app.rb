f723cd5a0d77ede5ff88144ea5539e641021b0a4
anandlakhani <anand.lakhani@gmail.com>
2015-12-10 17:24:05 -0500

    Update app.rb

--- honeywell-oauth-redirect-master/app.rb
+++ honeywell-oauth-redirect-master/app.rb
@@ -6,15 +6,18 @@ before do
 	content_type 'application/json'
 end
 
-get '/' do
+get '/redirect' do
+begin
+
   "Hello World!"
   code = params[:code]
-  
-  auth = ENV['basic_auth']
-  
-  response = RestClient.get( 'http://www.rediff.com')
-  response.code
- 
+  @auth = ENV['basic_auth']
+  auth_get = 'grant_type=authorization_code&code=' + code + '&redirect_uri=' + ENV['redirect_uri']
+  response = RestClient.post('https://qtccna.honeywell.com/sandbox/Auth/OAuth/token', auth_get, {:authorization => @auth, :accept => 'json', :content_type => 'application/x-www-form-urlencoded'})
+  response = JSON.parse(response)
+rescue => e
+   puts "ERROR: #{e}"
+ end 
 end
 
 
