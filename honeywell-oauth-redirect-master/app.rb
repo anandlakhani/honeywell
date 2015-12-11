da4af6c92c9528737d2ca76479548875b3a0176c
anandlakhani <anand.lakhani@gmail.com>
2015-12-11 15:12:46 -0500

    Update app.rb

--- honeywell-oauth-redirect-master/app.rb
+++ honeywell-oauth-redirect-master/app.rb
@@ -8,7 +8,7 @@ get '/redirect' do
   code = params[:code]
   @auth = ENV['basic_auth']
   auth_get = 'grant_type=authorization_code&code=' + code + '&redirect_uri=https://honeywell.herokuapp.com/redirect'
-  response = RestClient::Request.execute method: post, url:'https://qtccna.honeywell.com/sandbox/Auth/OAuth/token', auth_get, :authorization => @auth, :accept => 'json', :content_type => 'application/x-www-form-urlencoded'
+  response = RestClient::Request.execute method: post, url:'https://qtccna.honeywell.com/sandbox/Auth/OAuth/token', :payload =>auth_get, :authorization => @auth, :accept => 'json', :content_type => 'application/x-www-form-urlencoded'
   response
 end
 
