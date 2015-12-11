a25f2e1b07adb388bd2c650b89c2e8ffa6b0ca1a
anandlakhani <anand.lakhani@gmail.com>
2015-12-11 10:14:41 -0500

    Update app.rb

--- honeywell-oauth-redirect-master/app.rb
+++ honeywell-oauth-redirect-master/app.rb
@@ -2,16 +2,6 @@ require 'rest-client'
 require 'sinatra'
 require 'json'
 
-before do
-	content_type 'application/json'
-end
-
-get '/redirect' do
-  code = params[:code]
-  @auth = ENV['basic_auth']
-  auth_get = 'grant_type=authorization_code&code=' + code + '&redirect_uri=' + ENV['redirect_uri']
-  response = RestClient.post('https://qtccna.honeywell.com/sandbox/Auth/OAuth/token', auth_get, {:authorization => @auth, :accept => 'json', :content_type => 'application/x-www-form-urlencoded'})
-  response = JSON.parse(response)
-  response.to_json ## Spits out the data to the page in json
-end
-
+res=RestClient.post 'http://www.rediff.com', params.to_json, :content_type => :json, :accept => :json
+result = JSON.parse(res)
+return result.to_json
