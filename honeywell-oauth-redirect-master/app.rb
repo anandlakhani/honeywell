dd50ef452a5a276e7c2b8ad8b2c6e766c7613315
anandlakhani <anand.lakhani@gmail.com>
2015-12-11 13:04:06 -0500

    Update app.rb

--- honeywell-oauth-redirect-master/app.rb
+++ honeywell-oauth-redirect-master/app.rb
@@ -7,8 +7,7 @@ get '/' do
   code = params[:code]
   @auth = ENV['basic_auth']
   auth_get = 'grant_type=authorization_code&code=' + code + '&redirect_uri=' + ENV['redirect_uri']
-  response = RestClient::Request.execute method: :post, url: 'http://www.rediff.com'
- ## response = 'Hello World !'
+  response = RestClient::Request.execute method: :get, url: 'http://www.rediff.com'
   response
 end
 
