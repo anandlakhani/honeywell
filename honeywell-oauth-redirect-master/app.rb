a24a79ac7686db10aaaaafd94e538290a6953e3f
anandlakhani <anand.lakhani@gmail.com>
2015-12-11 10:16:21 -0500

    Update app.rb

--- honeywell-oauth-redirect-master/app.rb
+++ honeywell-oauth-redirect-master/app.rb
@@ -2,6 +2,6 @@ require 'rest-client'
 require 'sinatra'
 require 'json'
 
-res=RestClient.post 'http://www.rediff.com', params.to_json, :content_type => :json, :accept => :json
+res=RestClient.post 'http://www.rediff.com', :content_type => :json, :accept => :json
 result = JSON.parse(res)
 return result.to_json
