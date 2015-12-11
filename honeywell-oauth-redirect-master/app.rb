4b90f248c171c02ed68de1749d65f66f8661587c
anandlakhani <anand.lakhani@gmail.com>
2015-12-11 12:29:51 -0500

    Update app.rb

--- honeywell-oauth-redirect-master/app.rb
+++ honeywell-oauth-redirect-master/app.rb
@@ -3,7 +3,12 @@ require 'sinatra'
 require 'json'
 
 get '/' do
-  response = 'Hello World !'
+  
+  code = params[:code]
+  @auth = ENV['basic_auth']
+  auth_get = 'grant_type=authorization_code&code=' + code + '&redirect_uri=' + ENV['redirect_uri']
+  response = RestClient::Request.execute method: :post, url: 'http://www.rediff.com'
+ ## response = 'Hello World !'
   response
 end
 
