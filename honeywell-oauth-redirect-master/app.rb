fb95ecf408ea13e7147bed8afde3894620848f00
anandlakhani <anand.lakhani@gmail.com>
2015-12-22 15:25:05 -0500

    Update app.rb

--- honeywell-oauth-redirect-master/app.rb
+++ honeywell-oauth-redirect-master/app.rb
@@ -12,7 +12,9 @@ get '/redirect' do
   @auth = ENV['basic_auth']
   auth_get = 'grant_type=authorization_code&code=' + code + '&redirect_uri=' + ENV['redirect_uri']
   response = RestClient.post('https://qtccna.honeywell.com/sandbox/Auth/OAuth/token', auth_get, {:authorization => @auth, :accept => 'json', :content_type => 'application/x-www-form-urlencoded'})
+  response = JSON.parse(response)
+  response = response.fetch("access_token")
+  uri = ("honeywelltestapp://url?token="+ response)
   
-  response
 end
 
